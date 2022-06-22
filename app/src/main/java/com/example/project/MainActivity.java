26baee584d0c01792102e2e8f4fff447f649afd4
Vadim <mr.dimov2001@mail.ru>
2022-06-22 20:32:37 +0300

    Navigate login to register

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController fragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragments = new Navigation.findNavController(this, R.id.host);
+        fragments.navigate(R.id.login);
     }
 }
--- app/src/main/java/com/example/project/Register.java
+++ app/src/main/java/com/example/project/Register.java
@@ -2,24 +2,18 @@ package com.example.project;
 
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResultCaller;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Login#newInstance} factory method to
+ * Use the {@link Register#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Login extends Fragment {
+public class Register extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +24,7 @@ public class Login extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Login() {
+    public Register() {
         // Required empty public constructor
     }
 
@@ -40,11 +34,11 @@ public class Login extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment login.
+     * @return A new instance of fragment Register.
      */
     // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
+    public static Register newInstance(String param1, String param2) {
+        Register fragment = new Register();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,20 +53,12 @@ public class Login extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-        Button signIn = this.getActivity().findViewById(R.id.signIn);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Navigate to register
-            }
-        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login, container, false);
+        return inflater.inflate(R.layout.register, container, false);
     }
 }
